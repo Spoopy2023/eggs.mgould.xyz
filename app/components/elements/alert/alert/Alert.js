bf25e48a4776b979b17d2b804db42a4630fdd353
Mac Gould <87833105+Spoopy2023@users.noreply.github.com>
2024-05-26 02:46:01 +0100

    Fix text colouring on alerts

--- app/components/elements/alert/alert/Alert.js
+++ app/components/elements/alert/alert/Alert.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ({ children }) {
     return (
-        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 dark:border-yellow-500 dark:bg-yellow-500/25 border-yellow-700 bg-yellow-700/25 text-black dark:text-white text-black'}>
+        <div className={'flex items-center border-l-8  rounded-md shadow px-4 py-3 dark:border-yellow-500 dark:bg-yellow-500/25 border-yellow-700 bg-yellow-700/25 text-black dark:text-white text-black'}>
             {children}
         </div>
     );
--- app/components/elements/alert/warning/Warning.js
+++ app/components/elements/alert/warning/Warning.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function ({ children }) {
     return (
-        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 dark:border-red-500 dark:bg-red-500/25 border-red-700 bg-red-700/25 text-black dark:text-white text-black'}>
+        <div className={'flex items-center border-l-8  rounded-md shadow px-4 py-3 dark:border-red-500 dark:bg-red-500/25 border-red-700 bg-red-700/25 text-black dark:text-white '}>
             {children}
         </div>
     );
