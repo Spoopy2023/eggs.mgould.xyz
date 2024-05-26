a108a779f6d93df6551a2e376babdc48111296f4
Mac Gould <87833105+Spoopy2023@users.noreply.github.com>
2024-05-26 02:47:32 +0100

    Fix info alert background on lightmode

--- app/components/elements/alert/info/Info.js
+++ app/components/elements/alert/info/Info.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ({ children }) {
     return (
-        <div className={'flex items-center border-l-8  rounded-md shadow px-4 py-3 dark:border-blue-500 dark:bg-blue-500/25 border-blue-700 bg-green-700/25 text-black dark:text-white text-black'}>
+        <div className={'flex items-center border-l-8  rounded-md shadow px-4 py-3 dark:border-blue-500 dark:bg-blue-500/25 border-blue-700 bg-blue-700/25 text-black dark:text-white text-black'}>
             {children}
         </div>
     );
