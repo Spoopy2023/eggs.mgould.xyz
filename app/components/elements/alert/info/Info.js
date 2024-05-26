77d8d9d097e37553128e0320f3756095bd0391c0
Mac Gould <87833105+Spoopy2023@users.noreply.github.com>
2024-05-26 01:52:07 +0100

    Build error patch part 1

--- app/components/elements/alert/info/Info.js
+++ app/components/elements/alert/info/Info.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-export default ({ children }) => {
+export default ({ children })  = () => {
     return (
         <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 border-blue-500 bg-blue-500/25 text-black dark:text-white'}>
             {children}
