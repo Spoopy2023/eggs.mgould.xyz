4082f5f3c4f6f925b357e04d2c45acb3fa9d0c3d
Mac Gould <87833105+Spoopy2023@users.noreply.github.com>
2024-05-26 02:43:28 +0100

    Update light-dark mode alert colours

--- app/components/elements/alert/info/Info.js
+++ app/components/elements/alert/info/Info.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ({ children }) {
     return (
-        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 border-green-500 bg-green-500/25 text-black dark:text-white'}>
+        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 dark:border-blue-500 dark:bg-blue-500/25 border-blue-700 bg-green-blue/25 text-black dark:text-white text-black'}>
             {children}
         </div>
     );
--- app/components/elements/alert/success/Success.js
+++ app/components/elements/alert/success/Success.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ({ children }) {
     return (
-        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 border-green-500 bg-green-500/25 text-black dark:text-white'}>
+        <div className={'flex items-center border-l-8 text-gray-50 rounded-md shadow px-4 py-3 dark:border-green-500 dark:bg-green-500/25 border-green-700 bg-green-700/25 text-black dark:text-white text-black'}>
             {children}
         </div>
     );
