8b9485ea57133f68f4a7c5788f30b4bee8677436
Mac Gould <87833105+Spoopy2023@users.noreply.github.com>
2024-07-27 00:48:16 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -97,20 +97,6 @@ export default function Home() {
             Voice server eggs
           </p>
         </a>
-        <a
-          href="/bots"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Bots{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Eggs for pre-made discord bots
-          </p>
-        </a>
         {/* <a
           href="/database"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
